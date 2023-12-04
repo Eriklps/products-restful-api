6a97545fa9717b0493efa2ec7b001fafd4ff455b
Eriklps <erikdslopes@gmail.com>
2023-12-04 16:03:34 -0300

    Created Controllers

---
+++ src/main/java/com/example/productsrestfulapi/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.productsrestfulapi.controllers;
+
+import com.example.productsrestfulapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+}
