80619ddbdfe9e1f8473fa0ed3779a538fe144552
Eriklps <erikdslopes@gmail.com>
2023-12-04 14:25:03 -0300

    Initial commit

---
+++ src/test/java/com/example/productsrestfulapi/ProductsRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
